2094b5a014f71f3c894e20b7cf1d7f98dac92e35
firelink <moondrop@jsmith-develop.com>
2021-10-30 18:25:20 -0400

    Modified? idk

--- main.go
+++ main.go
@@ -1,34 +1,51 @@
 package api
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 )
 
-//Controller struct to handle API requests
-type Controller struct{}
-
-//ParseURL parses the URL
-func (c *Controller) ParseURL(url string) string {
-	return ("{\"path\":\"" + url + "\"}")
+//API Item and response struct, for returning our API data. API items is mutable
+type APIResponse struct {
+	status string
+	code   int
+	items  map[string]string
 }
 
 //Our API endpoints
 type API struct {
 	endpoint string
-	function func(w http.ResponseWriter)
+	function func(w http.ResponseWriter) *APIResponse
 }
 
 type APIEndpoint struct {
 	endpoints []API
 }
 
-func (e *APIEndpoint) AddNewEndpoint(end string, function func(w http.ResponseWriter)) {
+func (e *APIEndpoint) AddNewEndpoint(end string, function func(w http.ResponseWriter) *APIResponse) {
 	a := API{endpoint: end, function: function}
 	e.endpoints = append(e.endpoints, a)
 }
 
-func (e *APIEndpoint) FindEndpoint(end string, w http.ResponseWriter) func(w http.ResponseWriter) {
+//Controller struct to handle API requests
+type Controller struct {
+	APIEndpoint APIEndpoint
+}
+
+//ParseURL parses the URL
+func (c *Controller) ParseURL(w http.ResponseWriter, url string) string {
+	io.WriteString(w, url)
+
+	//TODO: Strip path
+	this_func := c.APIEndpoint.FindEndpoint(url, w)
+
+	response, _ := json.Marshal(this_func(w))
+
+	return string(response)
+}
+
+func (e *APIEndpoint) FindEndpoint(end string, w http.ResponseWriter) func(w http.ResponseWriter) *APIResponse {
 	for _, value := range e.endpoints {
 		io.WriteString(w, value.endpoint)
 		if value.endpoint == end {
@@ -39,24 +56,20 @@ func (e *APIEndpoint) FindEndpoint(end string, w http.ResponseWriter) func(w htt
 	return e.BaseFunction
 }
 
-func (e *APIEndpoint) BaseFunction(w http.ResponseWriter) {
+func (e *APIEndpoint) BaseFunction(w http.ResponseWriter) *APIResponse {
 	io.WriteString(w, "Hello World!")
+
+	return &APIResponse{}
 }
 
 //Listens and finds the route
 type Router struct {
-	Controller  Controller
-	APIEndpoint APIEndpoint
+	Controller Controller
 }
 
 func (a *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	data := a.Controller.ParseURL(r.URL.Path)
-
-	io.WriteString(w, data)
-	this_func := a.APIEndpoint.FindEndpoint(data, w)
-
-	this_func(w)
+	a.Controller.ParseURL(w, r.URL.Path)
 }
